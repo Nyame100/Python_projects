73a29a756c225e50a03796127b55b91f2d114b9f
Nyame100 <nyamerichmond100@gmail.com>
2026-01-31 22:51:53 +0100

    adding comments and playing around

--- PassGen.py
+++ PassGen.py
@@ -7,6 +7,7 @@ nr_letters = int(input("How many letters would you like in  your passowrd ?\n"))
 nr_symbols = int(input("How many symbols would you like ?\n"))
 nr_numbers = int(input("How many numbers would you like ?\n"))
 
+# List of alphabets, numbers and symbols.
 list_alpha = list(string.ascii_lowercase+string.ascii_uppercase)
 list_num = list(range(0,10))
 list_symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -16,6 +17,7 @@ password = ''
 # Getting the letters.
 letters = ''
 for i in range(nr_letters):
+#    letters += list_alpha[random.randint(0,51)] - alt. to random.choice() method.
     letters += random.choice(list_alpha)
 
 # Getting the symbols.
@@ -31,7 +33,12 @@ for i in range(nr_numbers):
 # Setting the unshuffled password.
 password = list(letters+symbols+numbers)
 
-# Shuffling the password
+# This is me avoiding to use the random.shuffle() method. Remove the ( )
+#(shuffledPassword = '')
+#(for i in range(len(password)):)
+#(    shuffledPassword += password[random.randint(0,len(password)-1)])
+
+# Shuffling the password.
 random.shuffle(password)
 
 # Converting passowrd back to a string.
