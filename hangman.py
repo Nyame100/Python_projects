901e67b5580795721e7ce0dd786745df0e92e3ce
Nyame100 <nyamerichmond100@gmail.com>
2026-02-17 00:56:43 +0100

    check the word against letter

--- hangman.py
+++ hangman.py
@@ -8,7 +8,17 @@ print(random_word)
 
 # Generate blanks matching each word.
 word_length = len(random_word)
-print("_"*word_length)
-
+blanks = list("_"*word_length)
+print(" ".join(blanks))
+print(blanks)
 # Asking user to guess a word.
-guess = input("Guess a letter\n")
+guessed_letter = input("Guess a letter\n").upper()
+
+# Checking if letter is in word.
+for i in range(len(random_word)):
+    i_occurence = []
+    if random_word[i] == guessed_letter:
+        i_occurence.append(i)
+        blanks[i] = guessed_letter
+        print(blanks)
+        print(" ".join(blanks))
